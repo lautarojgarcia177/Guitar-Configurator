d54fdec68ea83c61cf6fbedc9a0b9cd2dc4dc36b
lautaro garcia <lautarojgarcia177@gmail.com>
2020-11-26 00:17:49 -0300

    avance

--- main.js
+++ main.js
@@ -1,29 +1,36 @@
-
-function drawBody() {
-
-}
-
-function drawNeck() {
-    
-}
-
-const Concrete = require("./concrete.min");
-
-var concreteContainer = document.getElementById('concreteContainer');
-
-var viewport = new Contrecte.Viewport({
-    width: 400,
-    height: 200,
-    container: concreteContainer
+document.getElementById('body-white').addEventListener('click', function () {
+  console.log('body white');  
 });
 
-var bodyBlue = new Concrete.Layer();
-var bodyWhite = new Concrete.Layer();
+document.getElementById('body-light-blue').addEventListener('click', function () {
+    console.log('body light blue');  
+});
 
-var neckMaple = new Concrete.Layer();
-var neckRosewood = new Concrete.Layer();
+// function drawBody() {
 
-viewport.add(body).add(neck);
+// }
 
-// var scene = viewport.scene;
-viewport.render();
+// function drawNeck() {
+
+// }
+
+// const Concrete = require("./concrete.min");
+
+// var concreteContainer = document.getElementById('concreteContainer');
+
+// var viewport = new Contrecte.Viewport({
+//     width: 400,
+//     height: 200,
+//     container: concreteContainer
+// });
+
+// var bodyBlue = new Concrete.Layer();
+// var bodyWhite = new Concrete.Layer();
+
+// var neckMaple = new Concrete.Layer();
+// var neckRosewood = new Concrete.Layer();
+
+// viewport.add(body).add(neck);
+
+// // var scene = viewport.scene;
+// viewport.render();
